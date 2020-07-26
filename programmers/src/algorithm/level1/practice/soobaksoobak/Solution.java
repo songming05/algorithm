a7ebbaedc643d2b0887e67570c251699bfb0dd17
ADMIN_SONG <ADMIN_SONG@DESKTOP-PNJC9D2>
2020-07-26 18:10:10 +0900

    프로그래머스 레벨1 문제풀이
    예산
    수박수박수박
    x만큼 간격이 있는 n개의 숫자

---
+++ programmers/src/algorithm/level1/practice/soobaksoobak/Solution.java
@@ -0,0 +1,34 @@
+package algorithm.level1.practice.soobaksoobak;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		//https://programmers.co.kr/learn/courses/30/lessons/12922?language=java
+		
+		/*
+		example
+		n	return
+		3	수박수
+		4	수박수박
+		*/
+		int input[] = {3,4};
+		
+		for(int idx : input) System.out.println(solution(idx));
+		
+
+	}
+
+	public static String solution(int n){
+		StringBuffer answer = new StringBuffer();
+		String[] sb = new String[n];
+		for(int i=0; i<sb.length; i++){
+			if(i%2==0) sb[i] = "수";
+			else sb[i]= "박";
+		}//for
+		for(String data : sb){
+			answer.append(data);
+		}//for extended
+		return answer.toString();
+	}
+
+}
