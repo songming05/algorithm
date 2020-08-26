e10065660355984eaaf54537318c6a43791551e7
songming05 <songming05@gmail.com>
2020-08-26 23:55:38 +0900

    크레인인형뽑기_2019카카오 겨울인턴십 문제풀이

--- programmers/src/programmers/level1/kakao/CranePickerMain.java
+++ programmers/src/programmers/level1/kakao/CranePickerMain.java
@@ -3,6 +3,8 @@ package programmers.level1.kakao;
 public class CranePickerMain {
 
 	public static void main(String[] args) {
+		//크레인인형뽑기 2019 카카오 개발자 겨울 인턴십
+		//https://programmers.co.kr/learn/courses/30/lessons/64061	
 		Solution solution = new Solution();
 		int[][] board = {{0,0,0,0,0}, {0,0,1,0,3}, {0,2,5,0,1}, {4,2,4,4,2}
 		, {3,5,1,3,1}};		
