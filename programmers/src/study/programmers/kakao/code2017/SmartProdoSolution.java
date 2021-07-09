65e370ae33b7ba3ac34fe830e873bde228c81163
songming05 <45953864+songming05@users.noreply.github.com>
2021-07-09 13:06:11 +0900

    Update SmartProdoSolution.java

--- programmers/src/study/programmers/kakao/code2017/SmartProdoSolution.java
+++ programmers/src/study/programmers/kakao/code2017/SmartProdoSolution.java
@@ -1,5 +1,14 @@
 package study.programmers.kakao.code2017;
 
+/**
+ * 그래프 개념
+ * https://www.zerocho.com/category/Algorithm/post/584b9033580277001862f16c
+ * 그래프 1.2차원 배열 2.연결리스트
+ *   2차원 배열은 버텍스(Vertex, 점, 노드)가 적을 때 유리
+ * 
+ * @author songming05
+ *
+ */
 public class SmartProdoSolution {
 
     public static SmartProdoSolution newInstance() {
@@ -10,8 +19,13 @@ public class SmartProdoSolution {
     {
         //int[][] answer;
         int[][] answer = {{0, 3}, {1, 2}, {0, 6}, {1, 4}};
+        
+        //LinkedList<E>
+        //Graph
 
         return answer;
     }
+    
+    
 
 }
