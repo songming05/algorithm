602255b9ebfd1ac7a55b37bcaa800daef8da7a41
songming05 <45953864+songming05@users.noreply.github.com>
2021-10-18 17:23:15 +0900

    Update FirstDimensionArrayMain.java

--- programmers/src/baekjoon/step5/FirstDimensionArrayMain.java
+++ programmers/src/baekjoon/step5/FirstDimensionArrayMain.java
@@ -8,10 +8,67 @@ public class FirstDimensionArrayMain {
     public static void main(String[] args) throws Exception {
 
         //최소, 최대
-        problem10818();
+        //problem10818();
+        
+        //최댓값
+    	//https://www.acmicpc.net/problem/2562
+    	//problem2562();
+    	
+    	//숫자의 개수
+    	//https://www.acmicpc.net/problem/2577
+    	problem2577();
     }
 
-    private static void problem10818() throws IOException {
+    private static void problem2577() throws NumberFormatException, IOException {
+    	BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        //MAX 1,000,000,000
+        int a = Integer.parseInt(bufferedReader.readLine());
+        int b = Integer.parseInt(bufferedReader.readLine());
+        int c = Integer.parseInt(bufferedReader.readLine());
+        
+        int result = a * b * c;
+        
+        String data = String.valueOf(result);
+        int[] numArray = new int[10];
+        for(int i=0; i<data.length(); i++) {
+        	int number = Integer.parseInt(data.substring(i, i+1));
+        	numArray[number] = numArray[number]+1;
+        }
+        
+        for(int i=0; i < numArray.length; i++) {
+        	bufferedWriter.append(String.valueOf(numArray[i]));
+        	bufferedWriter.newLine();
+        }
+    	bufferedWriter.flush();
+	}
+
+	private static void problem2562() throws IOException {
+    	BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int place = 0;
+        int max = 0;
+        int count = 1;
+        String input = bufferedReader.readLine();
+        while(input != null ) {
+        	if("".equals(input)) break;
+        	int data = Integer.parseInt(input);
+        	if(max < data) {
+        		max = data;
+        		place = count;
+        	}
+        	
+        	count++;
+        	input = bufferedReader.readLine();
+        }
+        
+        System.out.println(max+"\n"+place);
+		
+	}
+
+	private static void problem10818() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
