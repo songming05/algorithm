93bc6a65b667cdf19ce64eef369aef9441192370
songming05 <45953864+songming05@users.noreply.github.com>
2021-11-10 00:31:03 +0900

    Update StringMain.java

--- programmers/src/baekjoon/step7/StringMain.java
+++ programmers/src/baekjoon/step7/StringMain.java
@@ -9,9 +9,25 @@ public class StringMain {
     public static void main(String[] args) throws Exception{
         //아스키코드
         //https://www.acmicpc.net/problem/11654
-        problem11654();
+        //problem11654();
+
+        //숫자의 합
+        //https://www.acmicpc.net/problem/11720
+        problem11720();
 
 
+    }
+
+    private static void problem11720() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        String[] datas = bufferedReader.readLine().split("");
+        int sum = 0;
+        for (int i = 0; i < datas.length; i++) {
+            sum += Integer.parseInt(datas[i]);
+        }
+        System.out.println(sum);
     }
 
     private static void problem11654() throws IOException {
@@ -19,6 +35,6 @@ public class StringMain {
 
         String input = bufferedReader.readLine();
         char[] asciis = input.toCharArray();
-        System.out.println(asciis[0]);
+        System.out.println((int) asciis[0]);
     }
 }
