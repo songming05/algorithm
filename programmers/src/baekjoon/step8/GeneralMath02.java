9129b64de8acfb8d0e7aab88c20161c2aebb05e5
songming05 <45953864+songming05@users.noreply.github.com>
2022-04-04 23:59:12 +0900

    Create GeneralMath02.java

---
+++ programmers/src/baekjoon/step8/GeneralMath02.java
@@ -0,0 +1,62 @@
+package baekjoon.step8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class GeneralMath02 {
+    public static void main(String[] args) throws Exception {
+        //소수찾기
+        //https://www.acmicpc.net/problem/1978
+        problem1978();
+    }
+
+    private static void problem1978() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //condition
+        // 1) n <= 100
+        // 2) number <= 1000
+        int n = Integer.parseInt(bufferedReader.readLine());
+        String[] datas = bufferedReader.readLine().split(" ");
+        int[] primes = findAllPrimeNumbers(1000);
+
+        int result = 0;
+        for (int i = 0; i < datas.length; i++) {
+            int number = Integer.parseInt(datas[i]);
+            for (int j = 0; j < primes.length; j++) {
+                if(number < primes[j] || primes[j] == 0) break; //해당 수가 소수가 아니거나 배열 초기값
+                if(number == primes[j]) {
+                    result++;
+                    break;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+
+    private static int[] findAllPrimeNumbers(int maxNumber) {
+        int[] primes = new int[maxNumber / 2]; //소수 배열
+        int count = 0;
+
+        primes[count++] = 2;
+        primes[count++] = 3;
+
+        for (int num = 5; num < maxNumber; num += 2) { //홀수만 체크
+
+            boolean isPrime = true;
+            //제곱근 이하의 어떤 소수로 나누어떨어지지 않으면 소수이다.
+            for (int i = 0; primes[i] * primes[i] <= num; i++) {
+                if(num % primes[i] == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) {
+                primes[count++] = num;
+            }
+        }
+
+        return primes;
+    }
+}
