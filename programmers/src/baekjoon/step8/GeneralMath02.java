7145b6cf5fc4e1095689134034d13974f91c5886
songming05 <45953864+songming05@users.noreply.github.com>
2022-04-12 00:00:22 +0900

    Update GeneralMath02.java

--- programmers/src/baekjoon/step8/GeneralMath02.java
+++ programmers/src/baekjoon/step8/GeneralMath02.java
@@ -13,7 +13,37 @@ public class GeneralMath02 {
 
         //소수
         //https://www.acmicpc.net/problem/2581
-        problem2581();
+        //problem2581();
+
+        //소인수분해
+        //https://www.acmicpc.net/problem/11653
+        problem11653();
+    }
+
+    private static void problem11653() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        switch (n) {
+            case 1: return;
+            case 2:
+            case 3:
+            case 5:
+            case 7: {
+                System.out.println(n);
+                return;
+            }
+            default: break;
+        }
+        int[] primes = findAllPrimeNumbers(n);
+        for (int i = 0; i < primes.length; i++) {
+            if(n % primes[i] == 0) {
+                System.out.println(primes[i]);
+                n = n / primes[i];
+                if(n == 1) break;
+                i--;
+                continue;
+            }
+        }
     }
 
     private static void problem2581() throws IOException {
@@ -69,7 +99,7 @@ public class GeneralMath02 {
         primes[count++] = 2;
         primes[count++] = 3;
 
-        for (int num = 5; num < maxNumber; num += 2) { //홀수만 체크
+        for (int num = 5; num <= maxNumber; num += 2) { //홀수만 체크
 
             boolean isPrime = true;
             //제곱근 이하의 어떤 소수로 나누어떨어지지 않으면 소수이다.
