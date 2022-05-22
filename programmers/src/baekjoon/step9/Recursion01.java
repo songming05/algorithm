cdb00064a82ecca36fa97bfcc04a45d94b32d1a1
songming05 <45953864+songming05@users.noreply.github.com>
2022-05-22 18:29:35 +0900

    Update Recursion01.java

--- programmers/src/baekjoon/step9/Recursion01.java
+++ programmers/src/baekjoon/step9/Recursion01.java
@@ -9,7 +9,23 @@ public class Recursion01 {
 
         //팩토리얼
         //https://www.acmicpc.net/problem/10872
-        problem10872();
+        //problem10872();
+
+        //피보나치 수 5
+        //https://www.acmicpc.net/problem/10870
+        problem10870();
+    }
+
+    private static void problem10870() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int result = pibonacheFive(n);
+        System.out.println(result);
+    }
+
+    private static int pibonacheFive(int n) {
+        if (n == 0 || n == 1) return n;
+        else return pibonacheFive(n-1) + pibonacheFive(n-2);
     }
 
     private static void problem10872() throws IOException {
