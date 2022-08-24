04e38cdc5c39cb593897caeb6fe49e05baf7a08c
songming05 <45953864+songming05@users.noreply.github.com>
2022-08-25 00:01:05 +0900

    Update UnionMap01.java

--- programmers/src/baekjoon/step12/UnionMap01.java
+++ programmers/src/baekjoon/step12/UnionMap01.java
@@ -3,6 +3,7 @@ package baekjoon.step12;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;
 
@@ -11,7 +12,30 @@ public class UnionMap01 {
     public static void main(String[] args) throws Exception {
         // 숫자 카드
         // https://www.acmicpc.net/problem/10815
-        problem10815();
+        // problem10815();
+
+        // 문자열 집합
+        // https://www.acmicpc.net/problem/14425
+        problem14425();
+    }
+
+    private static void problem14425() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] datas = bufferedReader.readLine().split(" ");
+        int n = Integer.parseInt(datas[0]);
+        int m = Integer.parseInt(datas[1]);
+        ArrayList<String> unionSList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            unionSList.add(bufferedReader.readLine());
+        }
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            if(unionSList.contains(bufferedReader.readLine())) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
     }
 
     private static void problem10815() throws IOException {
